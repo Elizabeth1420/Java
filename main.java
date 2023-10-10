7c9c8ba043ce7124f5642e6ecf74c9643281ed5a
Elizabeth Adeleke <elizabethadeleke683@gmail.com>
2023-10-10 10:59:53 +0100

    Imported AgeCalculator

---
+++ main.java
@@ -0,0 +1,17 @@
+/*(import java.util.Scanner; //import the Scanner class 
+import java.util.Date;
+import java.time.LocalDate;
+
+public class AgeCalculator {
+     public static void main(String []args){
+         Scanner obj = new Scanner(System.in);
+         int age;
+         int currentyear = 2023;
+         //Date cday;
+        System.out.println("Enter age: ");
+        age = obj.nextInt();
+        int birthyear = currentyear - age; 
+        //System.out.println("birth year is: " birthyear); 
+        System.out.printf("birth year is %s!%n", birthyear); 
+     }
+}
