14fe0eebab2146b18f8b6b68c9d1b050537614ab
Elizabeth Adeleke <elizabethadeleke683@gmail.com>
2023-11-13 22:18:13 +0000

    myproducts prints

--- main.java
+++ main.java
@@ -1,18 +1,9 @@
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Product myProduct = new Product( 222384, "Disco Ball", "Not Sure");    
+        product.print();
 
-public class main {
-    public static void main(String []args){
-      Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter length of rectangle");
-      
-      int length = scanner.nextInt();
-      System.out.println("Enter height of rectangle");
-      
-      int height = scanner.nextInt();
-      
-        scanner.close();
+        
 
+        }
     }
-    
-}
-
