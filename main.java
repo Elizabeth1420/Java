26582d753cdf025ccff887e2deee34003bec2efc
Elizabeth Adeleke <elizabethadeleke683@gmail.com>
2023-11-13 22:23:11 +0000

    tidy up file

--- main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Product myProduct = new Product( 222384, "Disco Ball", "Not Sure");    
-        product.print();
-
-        
-
-        }
-    }
