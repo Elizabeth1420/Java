b8bf7dd469b34ce5c4927eafafc93c476be3e788
Elizabeth1420 <66313212+Elizabeth1420@users.noreply.github.com>
2023-11-13 22:07:31 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Product myProduct = new Product( 222384, "Disco Ball", "Not Sure");    
+        product.print();
+
+        
+
+        }
+    }
